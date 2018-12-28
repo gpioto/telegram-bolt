297b166fb7a3ef1b03e7e45a1058201207e4383d
Gabriel Pioto <gabriel.piotogomes@gmail.com>
2018-12-28 18:28:21 -0200

    heroku port binding

--- index.js
+++ index.js
@@ -66,3 +66,10 @@ bot.command('quemfalta', ctx => {
     })
 })
 bot.startPolling()
+
+//Heroku needs to bind to a port
+const http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Node.JS!');
+}).listen(process.env.PORT || 5000);

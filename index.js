0178edd5202df17e108cc52f36235de042412389
gabriel.piotogomes <gabriel.piotogomes@gmail.com>
2019-12-31 02:51:27 +0000

    improved bot

--- index.js
+++ index.js
@@ -2,23 +2,22 @@ const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
 const _ = require('lodash')
-
-const keyboard = Markup.inlineKeyboard([
-  Markup.urlButton('Não Entre', 'https://bit.ly/IEN3qB'),
-  Markup.urlButton('Nem Pensar', 'https://bit.ly/1g5mUA0'),
-  Markup.urlButton('Também Não', 'https://bit.ly/IrygzT'),
-  Markup.urlButton('Nope!', 'https://bit.ly/IPSEup'),
-], {columns: 2});
+let db = {}
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply(`Parabéns ${ctx.from.first_name} e seja Bem Vindo ao Bolt! 
-Tudo já está funcionando, você não precisa fazer mais nada, mas se quiser pode tentar uma das quatro opções abaixo:`, Extra.markup(keyboard)) );
+bot.start((ctx) => ctx.reply(`Parabéns ${ctx.from.first_name} e seja Bem Vindo ao Bolt!
+O que você quer ganhar no amigo álcool?`));
+
+bot.on('message', (ctx) => {
+    db[ctx.chat.username] = ctx.message
+    ctx.reply(`Parabens, voce escolheu ${ctx.message}`)
+})
 
 bot.command('sortear', ctx => {
     const chatId = ctx.chat.id;
 
     if(ctx.chat.type == 'private') {
-        ctx.reply('Esse comando é pra ser rodado num grupo, sua anta!');
+        ctx.reply('Esse comando é pra ser rodado num grupo!');
         return;
     }
 
@@ -30,15 +29,20 @@ bot.command('sortear', ctx => {
         const shuffled = _.shuffle(users);
         let promises = []
         for (let i = 0; i < length; i++) {
-            const next = shuffled[(i+1)%length];
-            promises.push(bot.telegram.sendMessage(shuffled[i].id, `Você tirou ${next.first_name} (@${next.username}). Vê se dá um presente decente motherfucker!`));
+            const next = shuffled[(i + 1) % length];
+            promises.push(
+                bot.telegram.sendMessage(
+                    shuffled[i].id,
+                    `Você tirou ${next.first_name} (@${next.username}). E essa pessoa quer ${db[next.username]}`
+                )
+            );
         }
         Promise.all(promises).then(
             values => ctx.reply('Sorteado com sucesso! Viu saporra funciona'),
             reason => ctx.reply('Tem nego que ainda não conversou comigo, essa não valeu')
         )
     }).catch( err => {
-        ctx.reply('Ô MEU CARALHO VIU!');
+        ctx.reply('DEU RUIM!');
         ctx.reply(err);
     });
 })
@@ -52,7 +56,7 @@ bot.command('quemfalta', ctx => {
                 .then(result => resolve(null))
                 .catch(reason => resolve(admin.user));
             })
-            
+
         })).then(users => {
             const allUsers = users.filter(user => user != null);
             if (allUsers.length > 0) {
@@ -60,7 +64,7 @@ bot.command('quemfalta', ctx => {
 
                 ctx.reply('Falta essas feras aí meu, olhaaa: ' + userNames);
             } else {
-                ctx.reply('Não falta ninguém porra, agora é só /sortear')
+                ctx.reply('Não falta ninguém :D, agora é só /sortear')
             }
         }).catch(console.error);
     })
